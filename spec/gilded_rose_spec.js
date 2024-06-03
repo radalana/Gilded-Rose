f0865f7393ed438f8f8fd7bb6625b0a46433f1eb
Svetlana <radalana415@gmail.com>
2024-06-03 18:46:38 +0200

    Add tests for update_quality

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,78 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
+  it("lowers quality", function() {
+    items = [ new Item("foo", 0, 1) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+  it("min boundary: lowers quality", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].quality).toEqual(0);
+  });
+  it("lowers quality by 2 after sell in day", function() {
+    items = [ new Item("foo", -1, 2) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+  it("min boundary: lowers quality by 2 after sell in day", function() {
+    items = [ new Item("foo", -1, 1) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
   });
 
+  it("sell day decreases", function() {
+    items = [ new Item("foo", -1, 1) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(-2);
+  });
+
+//Aged Brie
+  it("Aged Brie getting better before sell in day", function() {
+    items = [ new Item("Aged Brie", 0, 49) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+  it("Aged Brie getting better after sell in day and max value", function() {
+    items = [ new Item("Aged Brie", -1, 50) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+  
+
+//Backstage passes
+  it("'Backstage passes' more than 10 days befor sell in", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 11, 49) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+  it("'Backstage passes' to a TAFKAL80ETC concert  between 10 and 5 days befor sell in", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+  it("'Backstage passes' between 5 and 1 days befor sell in", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+  it("'Backstage passes' is never more than 50", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 6, 47) ];
+    update_quality();
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+  it("'Backstage passes' in a day sell in and after", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 47) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+  //"Sulfuras":
+  it("'Sulfuras' quality and sellIn don't change", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
+    update_quality();
+    expect(items[0].quality).toEqual(80);
+    expect(items[0].sell_in).toEqual(0);
+  });
 });
+
